de07c4707411f55ad9e97a30b13ad5152adfacda
Tasi <tasi87@gmail.com>
2025-05-27 19:25:04 +0200

    Matematicke operacie.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
         String pozdrav = "Hello, world!";
 
         // Toto je pre vypísanie textu
         System.out.println(pozdrav);
-        System.out.println(pozdrav);
-        System.out.println(pozdrav);
-        System.out.println(pozdrav);
-
-
-
-        /*
-        Toto je viacriadkovy komentár
         */
 
+        int prveCislo = 10;
+        int druheCislo = 5;
+
+        prveCislo = druheCislo * 5;
+
+        System.out.println(prveCislo + druheCislo);
+        System.out.println(prveCislo - druheCislo);
+        System.out.println(prveCislo * druheCislo);
+        System.out.println(prveCislo / druheCislo);
     }
 }
