725bf7b6823a1fac9c378718750b19b93810c04e
Tasi <tasi87@gmail.com>
2025-05-27 18:56:06 +0200

    Pridal som komentare.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        // Toto je pre vypísanie textu
         System.out.println("Ahoj, svet!");
 
-
+        /*
+        Toto je viacriadkovy komentár
+        */
 
     }
 }
