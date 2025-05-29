2bee7449d4825814b574b381af95c4c0574f6c7f
Tasi <tasi87@gmail.com>
2025-05-29 19:22:45 +0200

    BMI kalkulacka.

--- src/Main.java
+++ src/Main.java
@@ -53,7 +53,7 @@ public class Main {
         System.out.println(!(cisloPorovnania > 8 || cisloPorovnania < 0));
         System.out.println(!true);
         */
-
+        /*
         // Feature pre kontrolu veku, či bude mať užívateľ viac ako 18 rokov
         int age;
         Scanner myScanner = new Scanner(System.in);
@@ -61,16 +61,46 @@ public class Main {
         System.out.println("Zadaj svoj vek:");
         age = myScanner.nextInt();
 
-        if (age >= 18 && age < 65) {
+        if (age >= 18) {
             System.out.println("Užívateľ je dospelý.");
-        } else if (age >= 65) {
-            System.out.println("Užívateľ je senior.");
-        } else if (age >=0 && age < 18) {
+            if (age >= 65) {
+                System.out.println("Užívateľ je senior.");
+            }
+        }  else if (age >=0 && age < 18) {
             System.out.println("Užívateľ nie je neplnoletý.");
         } else {
             System.out.println("Nejde zadať záporný vek");
         }
+        */
 
+        // Vypocet BMI (hmotnost/vyska)*2
+
+        // Variables
+        float height;
+        float weight;
+        float bmi;
+
+        // Scanner
+        Scanner myScanner = new Scanner(System.in);
+
+        // System Out
+        System.out.println("Zadaj svoju výšku(m):");
+        height = myScanner.nextFloat();
+        System.out.println("Zadaj svoju váhu(kg):");
+        weight = myScanner.nextFloat();
+        System.out.println((height / weight)*2);
+
+        // Calculation
+        bmi = weight / (height * height);
+
+        // Terms
+        if (bmi < 18.5f) {
+            System.out.println("Podváha");
+        } else if (bmi >= 18.5f && bmi > 25) {
+            System.out.println("Optimálna váha");
+        } else {
+            System.out.println("Nadváha");
+        }
 
 
     }
